3c359107d8f72dffc06350c4d2db6a14d31cd532
Parth Aadthakkar <parthadr@gmail.com>
2025-04-19 10:00:48 +0530

    Fixed loader.js

--- loader.js
+++ loader.js
@@ -2,7 +2,7 @@ function loader() {
     document.querySelector('.loader').style.display = 'none';
 }
 function fadeOut() {
-    setInterval(loader, 3000);
+    setInterval(loader, 2000);
 }
 
 window.onload = fadeOut();
